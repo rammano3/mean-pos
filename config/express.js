0792eee7acd6394875c02e67d06bd8dd91f0e400
rammano3 <randall.tyler.jones@gmail.com>
2014-01-13 17:23:56 -0600

    removing pretty console log module i created, dont need

--- config/express.js
+++ config/express.js
@@ -5,7 +5,6 @@ var express = require('express'),
     mongoStore = require('connect-mongo')(express),
     flash = require('connect-flash'),
     helpers = require('view-helpers'),
-    logp = require('consolelogpretty'),
     config = require('./config');
 
 module.exports = function(app, passport) {
